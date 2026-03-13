60e44c4e1d78328c82b283cd02960a5f0e673c67
emmannem <emmanuelriv15@gmail.com>
2026-03-13 00:08:37 -0600

    add modifieds

--- src/app/components/multas/multas.ts
+++ src/app/components/multas/multas.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { BibliotecaService } from '../../services/biblioteca.service';
 @Component({
   selector: 'app-multas',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './multas.html',
   styleUrl: './multas.css',
 })
 export class Multas {
-
+  constructor(public biblioteca: BibliotecaService) {}
 }
--- src/app/components/prestamos/prestamos.ts
+++ src/app/components/prestamos/prestamos.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { BibliotecaService } from '../../services/biblioteca.service';
 @Component({
   selector: 'app-prestamos',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './prestamos.html',
   styleUrl: './prestamos.css',
 })
 export class Prestamos {
+  // Variables ligadas a los inputs mediante [(ngModel)]
+  usuario: string = '';
+  titulo: string = '';
 
+  constructor(public biblioteca: BibliotecaService) {}
+
+  prestamo() {
+    if (this.usuario && this.titulo) {
+      // Llamamos al método del servicio
+      const exito = this.biblioteca.solicitarPrestamo(this.usuario, this.titulo);
+
+      if (exito) {
+        alert('Préstamo realizado con éxito');
+        // Limpiamos los campos después de la operación
+        this.usuario = '';
+        this.titulo = '';
+      } else {
+        alert('No se pudo realizar el préstamo. Verifica si el libro existe en el catálogo.');
+      }
+    } else {
+      alert('Por favor, completa ambos campos.');
+    }
+  }
 }
